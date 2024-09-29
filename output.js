6b15a417fd523238b5ab9742efde7700582d4c07
action <action@github.com>
2024-09-29 12:41:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Sep 29 2024 11:53:23 GMT+0000 (Coordinated Universal Time)
+//Sun Sep 29 2024 12:41:19 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 var _0xod5 = "jsjiami.com.v7";
